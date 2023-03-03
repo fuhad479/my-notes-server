be08b790e14bb711c0bb246abfed867f4cf74539
fuhad479 <fuhadhasan479@gmail.com>
2023-03-03 16:09:47 +0600

    established a connection with database

---
+++ database/connect.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const client = new MongoClient(process.env.CONNECTION_STRING);
+
+async function connect() {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const https = require("https");
 const fs = require("fs");
 
+const connect = require("./database/connect");
+
+dotenv.config();
+
 // this is express app object
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 // making a https server
 const server = https.createServer(
@@ -11,5 +18,15 @@ const server = https.createServer(
   app
 );
 
-// starting the https server
-server.listen(5000, () => console.log(`http://localhost:5000`));
+// this function will establish a connection to the database start the express server
+const establishConnection = async () => {
+  try {
+    // connect with database
+    await connect();
+    // starting the https server
+    server.listen(port, () => console.log(`https://localhost:${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+establishConnection();
